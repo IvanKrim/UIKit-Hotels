1c7590ac08cd54b5021f3a426ba8fb1d9bf7e52e
KrimGit <reamonn96@gmail.com>
2022-01-12 17:32:13 +0300

    Development branch created

---
+++ UIKit-Hotels/Model/Hotel.swift
@@ -0,0 +1,29 @@
+//
+//  Hotel.swift
+//  UIKit-Hotels
+//
+//  Created by Kalabishka Ivan on 12.01.2022.
+//
+
+import Foundation
+
+struct Hotel: Decodable {
+    let id: Int
+    let name: String
+    let address: String
+    let stars: Double
+    let distance: Double
+    let suitesAvailability: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, address, stars, distance
+        case suitesAvailability = "suites_availability"
+    }
+}
+
+typealias Hotels = [Hotel]
+
+enum ApiStrings: String {
+    case generalApi = "https://raw.githubusercontent.com/iMofas/ios-android-test/master/0777.json"
+}
+
---
+++ UIKit-Hotels/Modules/HotelsListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HotelsListViewController.swift
+//  UIKit-Hotels
+//
+//  Created by Kalabishka Ivan on 12.01.2022.
+//
+
+import Foundation
+
+import UIKit
+
+class HotelsListViewController: UIViewController {
+
+}
