4c6b4b088d050d2dcb0f09b2be7643ba5aa70bd3
KrimGit <reamonn96@gmail.com>
2022-01-12 17:18:03 +0300

    Initial Commit

---
+++ UIKit-Hotels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit-Hotels
+//
+//  Created by Kalabishka Ivan on 12.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
